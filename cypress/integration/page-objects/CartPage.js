c0edbd3e07ce6ac46cf7103d09deee4d99ede83a
praveen <kmpraveen25@gmail.com>
2023-01-18 13:03:24 +0000

    updated cypress folder

---
+++ cypress/integration/page-objects/CartPage.js
@@ -0,0 +1,14 @@
+class CartPage {
+    getCartFooter() {
+        return cy.get(".cart_footer");
+    }
+
+    getCartItem() {
+        return cy.get(".inventory_item_name");
+    }
+
+    getCheckoutButton() {
+        return cy.get("#checkout")
+    }
+}
+export default new CartPage();
